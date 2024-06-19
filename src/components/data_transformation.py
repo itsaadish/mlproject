8cb490cf5c26ea41368e75915a6712de0733fc0e
Aadish Sharma <Aadish@Aadishs-MacBook-Pro.local>
2024-06-19 11:05:40 +0530

    data ingestion

---
+++ src/components/data_transformation.py
@@ -0,0 +1,12 @@
+# Python program to explain os.getcwd() method 
+		
+# importing os module 
+import os 
+	
+# Get the current working 
+# directory (CWD) 
+cwd :str = os.path.join('artifact','train.csv')
+	
+# Print the current working 
+# directory (CWD) 
+print("Current working directory:", cwd) 
